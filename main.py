c8aa61eef63a1c4fbaa8f4d26e40aaf91db216c0
Владимир <baranov-v-k@yandex.ru>
2025-01-04 09:41:46 +0300

    Отредактирована стилизация карточек, чтобы класс product list оборачивал только 3 и более карточек

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from db_directory.db_config import db_user, db_password, db_name
 from pages_directory.catalog_page import catalog_route
 from pages_directory.home_page import home_route
 from pages_directory.personal_account_page import personal_account_route
+from pages_directory.product_page import product_page_route
 from pages_directory.reg_and_auth_page import reg_and_auth_route
 from pages_directory.user_data_changing_page import user_data_changing_route
 
@@ -20,7 +21,7 @@ app.include_router(user_crud_rout)
 app.include_router(reg_and_auth_route)
 app.include_router(personal_account_route)
 app.include_router(user_data_changing_route)
-
+app.include_router(product_page_route)
 register_tortoise(
     app,
     db_url=f'asyncpg://{db_user}:{db_password}@127.0.0.1:5432/{db_name}',
--- pages_directory/catalog_page.py
+++ pages_directory/catalog_page.py
@@ -10,14 +10,14 @@ templates = Jinja2Templates(directory="templates")
 
 
 @catalog_route.get("/catalog/", response_class=HTMLResponse)
-async def catalog(request: Request):
+async def catalog_get(request: Request):
     all_products = await Product.all()
     return templates.TemplateResponse("catalog_page.html", {"request": request,
                                                             "all_products": all_products, })
 
 
 @catalog_route.get("/catalog/{session_id}/{user_id}/", response_class=HTMLResponse)
-async def catalog_by_id(request: Request, session_id: str, user_id: int):
+async def catalog_by_user_id_get(request: Request, session_id: str, user_id: int):
     user_for_check = await User.get_or_none(id=user_id, session_id=session_id)
     if not user_for_check:
         raise HTTPException(403, 'Доступ запрещён')
@@ -25,3 +25,13 @@ async def catalog_by_id(request: Request, session_id: str, user_id: int):
     return templates.TemplateResponse("catalog_page.html", {"request": request,
                                                             "session_id": session_id,
                                                             "user_id": user_id})
+
+
+@catalog_route.get("/catalog_filter_by_gender/{gender}/", response_class=HTMLResponse)
+async def catalog_gender_filter_get(request: Request, gender: str):
+    filtered_products = await Product.filter(gender=gender)
+    len_filtered_products = len(filtered_products)
+    return templates.TemplateResponse("catalog_by_gender_page.html", {"request": request,
+                                                                      "gender": gender,
+                                                                      "len_filtered_products": len_filtered_products,
+                                                                      "filtered_products": filtered_products, })
