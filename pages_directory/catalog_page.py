dee7e462403288fcc0e9348b839a5df80405d604
Владимир <baranov-v-k@yandex.ru>
2025-01-04 20:29:28 +0300

    Добавление всех новых ф-ций для авторизованного пользователя

--- pages_directory/catalog_page.py
+++ pages_directory/catalog_page.py
@@ -52,9 +52,48 @@ async def catalog_by_user_id_get(request: Request, session_id: str, user_id: int
     if not user_for_check:
         raise HTTPException(403, 'Доступ запрещён')
 
+    split_products_lists = None
+    products_per_page = 8
+    page_number = 0
+    available_pages = []
+
+    all_products = await Product.all()
+    if len(all_products) > products_per_page:
+        split_products_lists = nested_lists_maker(all_products, products_per_page)
+        for i, val in enumerate(split_products_lists):
+            available_pages.append(i)
+
     return templates.TemplateResponse("catalog_page.html", {"request": request,
+                                                            "split_products_lists": split_products_lists,
+                                                            "products_per_page": products_per_page,
+                                                            "available_pages": available_pages,
+                                                            "page_number": page_number,
+                                                            "all_products": all_products,
                                                             "session_id": session_id,
                                                             "user_id": user_id})
+
+
+@catalog_route.get("/catalog/{session_id}/{user_id}/paginator/{products_per_page}/{page_number}/",
+                   response_class=HTMLResponse)
+async def catalog_paginator_by_user_id_get(request: Request, session_id: str, user_id: int, products_per_page: int,
+                                           page_number: int):
+    user_for_check = await User.get_or_none(id=user_id, session_id=session_id)
+    if not user_for_check:
+        raise HTTPException(403, 'Доступ запрещён')
+
+    available_pages = []
+    all_products = await Product.all()
+    split_products_lists = nested_lists_maker(all_products, products_per_page)
+    for i, val in enumerate(split_products_lists):
+        available_pages.append(i)
+
+    return templates.TemplateResponse("catalog_paginator_page.html", {"request": request,
+                                                                      "products_per_page": products_per_page,
+                                                                      "page_number": page_number,
+                                                                      "available_pages": available_pages,
+                                                                      "split_products_lists": split_products_lists,
+                                                                      "session_id": session_id,
+                                                                      "user_id": user_id})
 
 
 @catalog_route.get("/catalog_filter_by_gender/{gender}/", response_class=HTMLResponse)
@@ -79,6 +118,34 @@ async def catalog_gender_filter_get(request: Request, gender: str):
                                                                       "filtered_products": filtered_products, })
 
 
+@catalog_route.get("/catalog/{session_id}/{user_id}/catalog_filter_by_gender/{gender}/", response_class=HTMLResponse)
+async def catalog_gender_filter_by_user_id_get(request: Request, session_id: str, user_id: int, gender: str):
+    user_for_check = await User.get_or_none(id=user_id, session_id=session_id)
+    if not user_for_check:
+        raise HTTPException(403, 'Доступ запрещён')
+
+    split_products_lists = None
+    products_per_page = 8
+    page_number = 0
+    available_pages = []
+
+    filtered_products = await Product.filter(gender=gender)
+    if len(filtered_products) > products_per_page:
+        split_products_lists = nested_lists_maker(filtered_products, products_per_page)
+        for i, val in enumerate(split_products_lists):
+            available_pages.append(i)
+
+    return templates.TemplateResponse("catalog_by_gender_page.html", {"request": request,
+                                                                      "split_products_lists": split_products_lists,
+                                                                      "products_per_page": products_per_page,
+                                                                      "available_pages": available_pages,
+                                                                      "page_number": page_number,
+                                                                      "gender": gender,
+                                                                      "filtered_products": filtered_products,
+                                                                      "session_id": session_id,
+                                                                      "user_id": user_id})
+
+
 @catalog_route.get("/catalog/paginator/{products_per_page}/{page_number}/{gender}/", response_class=HTMLResponse)
 async def catalog_paginator_by_gender_get(request: Request, products_per_page: int, page_number: int, gender: str):
     available_pages = []
@@ -94,3 +161,27 @@ async def catalog_paginator_by_gender_get(request: Request, products_per_page: i
                                                                                 "available_pages": available_pages,
                                                                                 "gender": gender,
                                                                                 "split_products_lists": split_products_lists, })
+
+
+@catalog_route.get("/catalog/paginator/{session_id}/{user_id}/{products_per_page}/{page_number}/{gender}/",
+                   response_class=HTMLResponse)
+async def catalog_paginator_by_gender_by_user_id_get(request: Request, session_id: str, user_id: int,
+                                                     products_per_page: int, page_number: int, gender: str):
+    user_for_check = await User.get_or_none(id=user_id, session_id=session_id)
+    if not user_for_check:
+        raise HTTPException(403, 'Доступ запрещён')
+
+    available_pages = []
+    filtered_products = await Product.filter(gender=gender)
+    split_products_lists = nested_lists_maker(filtered_products, products_per_page)
+    for i, val in enumerate(split_products_lists):
+        available_pages.append(i)
+
+    return templates.TemplateResponse("catalog_paginator_by_gender_page.html", {"request": request,
+                                                                                "products_per_page": products_per_page,
+                                                                                "page_number": page_number,
+                                                                                "available_pages": available_pages,
+                                                                                "gender": gender,
+                                                                                "split_products_lists": split_products_lists,
+                                                                                "session_id": session_id,
+                                                                                "user_id": user_id})
--- pages_directory/product_page.py
+++ pages_directory/product_page.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+import json
+
+from fastapi import APIRouter, Form
 from fastapi import Request, HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
@@ -14,3 +16,37 @@ async def product_by_id_get(request: Request, product_id: int):
     product_by_id = await Product.get(id=product_id)
     return templates.TemplateResponse("product_page.html", {"request": request,
                                                             "product_by_id": product_by_id, })
+
+
+@product_page_route.get("/product_details/{session_id}/{user_id}/{product.id}/", response_class=HTMLResponse)
+async def product_by_self_and_user_id_get(request: Request, session_id: str, user_id: int, product_id: int):
+    user_for_check = await User.get_or_none(id=user_id, session_id=session_id)
+    if not user_for_check:
+        raise HTTPException(403, 'Доступ запрещён')
+
+    product_by_id = await Product.get(id=product_id)
+    return templates.TemplateResponse("product_page.html", {"request": request,
+                                                            "product_by_id": product_by_id, })
+
+
+@product_page_route.post("/product_details/{session_id}/{user_id}/{product_id}/", response_class=HTMLResponse)
+async def product_by_self_and_user_id_post(request: Request, session_id: str, user_id: int, product_id: int,
+                                           product_amount: int = Form()):
+    user_for_check = await User.get_or_none(id=user_id, session_id=session_id)
+    if not user_for_check:
+        raise HTTPException(403, 'Доступ запрещён')
+
+    product_by_id = await Product.get(id=product_id)
+    total_orders = 0
+    if user_for_check.orders:
+        total_orders += max(user_for_check.orders)
+        user_for_check.orders = user_for_check.orders | json.dumps({total_orders: {"product_id": product_id,
+                                                                                   "product_amount": product_amount}})
+    else:
+        user_for_check.orders = json.dumps({total_orders: {"product_id": product_id,
+                                                           "product_amount": product_amount}})
+    await user_for_check.save()
+
+    return RedirectResponse(f'/product_details/{session_id}/{user_id}/{product_id}/')
+    # return templates.TemplateResponse("product_page.html", {"request": request,
+    #                                                         "product_by_id": product_by_id, })
