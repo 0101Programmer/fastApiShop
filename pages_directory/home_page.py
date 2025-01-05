d119612fd796406913a3b955aca1b14e214b819d
Владимир <baranov-v-k@yandex.ru>
2025-01-05 16:28:48 +0300

    Графические улучшения

--- pages_directory/home_page.py
+++ pages_directory/home_page.py
@@ -39,3 +39,19 @@ async def home_by_id_get(request: Request, session_id: str, user_id: int):
     return templates.TemplateResponse("home_page.html", {"request": request,
                                                          "session_id": session_id,
                                                          "user_id": user_id})
+
+
+@home_route.get("/contacts", response_class=HTMLResponse)
+async def about_us_get(request: Request):
+    return templates.TemplateResponse("about_us_page.html", {"request": request})
+
+
+@home_route.get("/contacts_by_id/{session_id}/{user_id}", response_class=HTMLResponse)
+async def about_us_by_id_get(request: Request, session_id: str, user_id: int):
+    user_for_check = await User.get_or_none(id=user_id, session_id=session_id)
+    if not user_for_check:
+        raise HTTPException(403, 'Доступ запрещён')
+
+    return templates.TemplateResponse("about_us_page.html", {"request": request,
+                                                        "session_id": session_id,
+                                                        "user_id": user_id})
